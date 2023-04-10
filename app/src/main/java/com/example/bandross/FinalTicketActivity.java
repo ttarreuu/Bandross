3039772c368f457dc7704764ee82c1a04d0b6d0a
MangoMbp <notwatermango@gmail.com>
2023-04-10 10:49:18 +0700

    passing ticket data OK

--- app/src/main/java/com/example/bandross/FinalTicketActivity.java
+++ app/src/main/java/com/example/bandross/FinalTicketActivity.java
@@ -2,13 +2,38 @@ package com.example.bandross;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FinalTicketActivity extends AppCompatActivity {
-
+    String name, date, time, type, passenger;
+    TextView tv_name, tv_date, tv_time, tv_type, tv_passenger, tv_totalPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_ticket);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+        date = intent.getStringExtra("date");
+        time = intent.getStringExtra("time");
+        type = intent.getStringExtra("type");
+        passenger = intent.getStringExtra("passenger");
+
+        tv_name = (TextView) findViewById(R.id.inputbox_name);
+        tv_date = (TextView) findViewById(R.id.inputbox_date);
+        tv_time = (TextView) findViewById(R.id.inputbox_time);
+        tv_type = (TextView) findViewById(R.id.inputbox_type);
+        tv_passenger = (TextView) findViewById(R.id.inputbox_passenger);
+        tv_totalPrice = (TextView) findViewById(R.id.inputbox_totalprice);
+
+        tv_name.setText(name);
+        tv_date.setText(date);
+        tv_time.setText(time);
+        tv_type.setText(type);
+        tv_passenger.setText(passenger);
+        // too lazy to handle input format, assume passenger is int parsable
+        tv_totalPrice.setText(Integer.toString(20000*(Integer.parseInt(passenger))));
     }
 }
--- app/src/main/java/com/example/bandross/TicketFragment.java
+++ app/src/main/java/com/example/bandross/TicketFragment.java
@@ -9,58 +9,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TicketFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TicketFragment extends Fragment {
 
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText et_name, et_date, et_time, et_passenger;
+    private Spinner et_type;
+    private Button btn_ticket;
 
     public TicketFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TicketFragment newInstance(String param1, String param2) {
-        TicketFragment fragment = new TicketFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticket, container, false);
+        View view = inflater.inflate(R.layout.fragment_ticket, container, false);
+
+        et_type = (Spinner) view.findViewById(R.id.inputbox_type);
+        et_name = (EditText) view.findViewById(R.id.inputbox_name);
+        et_date = (EditText) view.findViewById(R.id.inputbox_date);
+        et_time = (EditText) view.findViewById(R.id.inputbox_time);
+        et_passenger = (EditText) view.findViewById(R.id.inputbox_passenger);
+        btn_ticket = (Button) view.findViewById(R.id.btn_book);
+
+        btn_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = et_name.getText().toString();
+                String type = et_type.getSelectedItem().toString();
+                String date = et_date.getText().toString();
+                String time = et_time.getText().toString();
+                String passenger = et_passenger.getText().toString();
+
+                Intent intent = new Intent(getActivity(), FinalTicketActivity.class);
+                // Pass data
+                intent.putExtra("name", name);
+                intent.putExtra("type", type);
+                intent.putExtra("date", date);
+                intent.putExtra("time", time);
+                intent.putExtra("passenger", passenger);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
